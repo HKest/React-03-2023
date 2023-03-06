d01be674aedbfd256de081f3e2441f7c3c3c6a19
HKest <117974299+HKest@users.noreply.github.com>
2023-03-06 22:41:54 +0200

    Projekti algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <img className="pilt" src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt="" />
+   <button className="nupp">Nupp</button>
+   {/* <p></p>
+   <h1>1</h1>
+   <div>Tekst</div> */}
+    </div>
+
+  );
+}
+
+export default App;
