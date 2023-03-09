5216f5910ba49a8f3d3fedcca9fec8c23e579c32
HKest <117974299+HKest@users.noreply.github.com>
2023-03-09 21:32:17 +0200

    Update App.js
    lisasin pealkirja

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <h1>SIIA TULEB VEEBIPOOD</h1>
    <img className="pilt" src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt="" />
    <button className="nupp">Nupp</button>
    {/* <p></p>
    <h1>1</h1>
    <div>Tekst</div> */}
+ 
     </div>
 
   );
